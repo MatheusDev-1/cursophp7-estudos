2a94ae2ec9653cd0988110e54b3b3a7f1ba609b2
Matheus Oliveira da Hora <36653020+MatheusDev-1@users.noreply.github.com>
2020-11-09 01:37:32 -0300

    chore: class examples commented

--- pdo/index.php
+++ pdo/index.php
@@ -2,32 +2,37 @@
 
 require_once("config.php");
 
+// Carrega por ID
 // $usuario = new Usuario();
 // $id = $usuario->loadById('1');
-// $lista = $usuario->getList();
 
+
+// Carrega todos os usuários
 //$lista = Usuario::getList();
 //echo json_encode($lista);
 
+// Procura por trecho no campo deslogin
 // $search = Usuario::search('em');
-
 // echo json_encode($search);
 
+// Procura registro que possua deslogin e dessenha corretos
 //$usuario->login("em12104", "brscan20");
-
 //echo $usuario;
 
+// Insere novos dados na tabela
 // $usuario = new Usuario("Matthew", "1234566");
 // $usuario->insert();
-
 // echo $usuario;
 
+// Delete dado da tabela através de ID
+// $usuario = new Usuario();
+// $usuario->loadById(1);
+// $usuario->delete();
+
+// Atualiza dado da tabela
 $usuario = new Usuario();
 $usuario->loadById(1);
-$usuario->delete();
-// $usuario->loadById(1);
-// $usuario->update("professor", "122234");
-
-// echo $usuario;
+$usuario->update("professor", "122234");
+echo $usuario;
 
 ?>
